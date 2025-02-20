8624d34fa968a4c511df1b750bb6be9ab9d678c5
Des Herriott <des.herriott@gmail.com>
2025-02-20 10:27:16 +0000

    feat: buffed the rift weaver boss a bit more
    Health 400 -> 1000
    Summon reinforcements after every 200 damage taken
    Constant slow regen in fights with 2 or more players

--- src/main/java/dev/ftb/mods/ftboceanmobs/registry/ModEntityTypes.java
+++ src/main/java/dev/ftb/mods/ftboceanmobs/registry/ModEntityTypes.java
@@ -4,11 +4,13 @@ import dev.ftb.mods.ftboceanmobs.FTBOceanMobs;
 import dev.ftb.mods.ftboceanmobs.entity.*;
 import dev.ftb.mods.ftboceanmobs.entity.riftweaver.RiftWeaverBoss;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 public class ModEntityTypes {
@@ -40,6 +42,22 @@ public class ModEntityTypes {
             = register("sludgeling", ModEntityTypes::sludgeling);
     public static final Supplier<EntityType<TumblingBlockEntity>> TUMBLING_BLOCK
             = register("tumbling_block", ModEntityTypes::tumblingBlock);
+
+    public static final List<Supplier<? extends EntityType<? extends BaseRiftMob>>> NORMAL_MOBS = List.of(
+            RIFTLING_OBSERVER,
+            ABYSSAL_WINGED,
+            CORROSIVE_CRAIG,
+            MOSSBACK_GOLIATH,
+            ABYSSAL_SLUDGE,
+            SHADOW_BEAST,
+            RIFT_MINOTAUR,
+            TENTACLED_HORROR,
+            RIFT_DEMON
+    );
+
+    public static EntityType<? extends BaseRiftMob> pickRandomMob(RandomSource randomSource) {
+        return NORMAL_MOBS.get(randomSource.nextInt(NORMAL_MOBS.size())).get();
+    }
 
     private static <E extends Entity> Supplier<EntityType<E>> register(final String name, final Supplier<EntityType.Builder<E>> sup) {
         return ENTITY_TYPES.register(name, () -> sup.get().build(name));
